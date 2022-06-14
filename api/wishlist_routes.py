a7a50838de1d119afb8942938ae941f3a9a45db7
Daniel <dmila012@fiu.edu>
2022-06-13 20:54:47 -0400

    Fixed GET and PUSH for acquiring GET wishlist/id, POST /user to create a dummy user and GET user/id to get dummy information and their wishlist based on their user id.

--- api/wishlist_routes.py
+++ api/wishlist_routes.py
@@ -14,15 +14,7 @@ import json
 from flask import request, jsonify, Blueprint
 
 # add your models to the models.py file then import them here
-<<<<<<< HEAD
-<<<<<<< HEAD
 from ..models import db, Book, Author, ma, BookSchema, User, Wishlist, ShoppingCart, WishlistSchema
-=======
-from ..models import db, Book, Author, ma, BookSchema
->>>>>>> b2bb61f (Added api wishlist route from blueprint)
-=======
-from ..models import db, Book, Author, ma, BookSchema, User
->>>>>>> 4fb2978 (Added wishlist route for POST and GET)
 from dateutil.parser import parse
 from http import HTTPStatus
 
@@ -34,8 +26,6 @@ api = Blueprint('wishlist_routes', __name__)
 
 # example route definition
 # the decorator below starts with `@api` because that what the blueprint was name on line 14
-<<<<<<< HEAD
-
 
 # Getting a wishlist by wishlist id
 @api.route("/wishlist/<id>", methods=['GET'])
@@ -140,39 +130,3 @@ def remove_book(id, isbn):
     shopping_cart_books = [book.as_dict() for book in shopping_cart.books]
 
     return jsonify({f"{user.username}'s Wishlist": books, "Book to Remove": book.as_dict(), f"{user.username}'s Shopping Cart": shopping_cart_books}), 200
-
-=======
-@api.route("/wishlist", methods=['GET'])
-@cache.cached(timeout=5) # add this decorator to cache data on GET routes (this one caches data for 5 seconds)
-def routeFunction():
-    return jsonify(message={"Success": f"Blueprint {api.name} configured!"})
-<<<<<<< HEAD
->>>>>>> b2bb61f (Added api wishlist route from blueprint)
-=======
-
-@api.route("/wishlist", methods=['POST'])
-def add_book():
-    book_id = request.json['book_id']
-    book = Book.query.get(book_id)
-
-    return jsonify(book=book.as_dict()), 200
-
-@api.route("/user", methods=['POST'])
-def add_user():
-    user = User(**request.json)
-
-    try:
-        db.session.add(user)
-        db.session.commit()
-    except Exception as e:
-        print(e)
-        return jsonify(msg={"Error":f"error {e}"}), 500
-
-    return jsonify(user.as_dict()), 200
-
-@api.route("/user/<id>", methods=['GET'])
-def get_user(id):
-    user = User.query.get(id)
-
-    return jsonify(msg=user.wishlist), 200
->>>>>>> 4fb2978 (Added wishlist route for POST and GET)
